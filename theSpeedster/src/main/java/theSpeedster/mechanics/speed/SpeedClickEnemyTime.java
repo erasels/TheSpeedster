9c1ea578657286c95cc5d15ea53d02c65e4103d3
erasels <emanuelbedburdick@yahoo.de>
2019-10-05 16:49:00 +0200

    Create SpeedClickEnemyTime.java

---
+++ theSpeedster/src/main/java/theSpeedster/mechanics/speed/SpeedClickEnemyTime.java
@@ -0,0 +1,32 @@
+package theSpeedster.mechanics.speed;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.WindyParticleEffect;
+import theSpeedster.util.AlchHelper;
+
+import java.util.function.Consumer;
+
+public class SpeedClickEnemyTime extends AbstractSpeedTime {
+    public SpeedClickEnemyTime(float duration, Consumer<AbstractMonster> effect) {
+        super(Location.BEHIND_MONSTER, duration);
+        effectAction = effect;
+    }
+
+    @Override
+    protected void effect() {
+        if(InputHelper.justClickedLeft) {
+            AbstractMonster m = AlchHelper.getClosestMonster(InputHelper.mX, InputHelper.mY, 300f);
+            if(m != null)
+                effectAction.accept(m);
+        }
+
+    }
+
+    @Override
+    protected void renderEffects(SpriteBatch sb) {
+        AbstractDungeon.effectsQueue.add(new WindyParticleEffect());
+    }
+}
