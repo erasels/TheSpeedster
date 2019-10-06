8fd70ea01265e17196f8b8ecf5f0f6d98537dd29
erasels <emanuelbedburdick@yahoo.de>
2019-10-06 12:56:52 +0200

    Hitbox intersection prevention

--- theSpeedster/src/main/java/theSpeedster/mechanics/speed/ButtonGenerators/BasicButtonGenerator.java
+++ theSpeedster/src/main/java/theSpeedster/mechanics/speed/ButtonGenerators/BasicButtonGenerator.java
@@ -14,9 +14,16 @@ public class BasicButtonGenerator extends AbstractButtonGenerator{
     public void logic() {
         timeSince -= UC.gt();
         if(timeSince < 0) {
-            float x = MathUtils.random(MIN_X, MAX_X);
-            float y = MathUtils.random(MIN_Y, MAX_Y);
-            buttons.add(new TimedButton(x, y, intensity*2f, clickEffect, false));
+            while(true) {
+                //Better left unseeded or it'd be easy to cheese
+                float x = MathUtils.random(MIN_X, MAX_X);
+                float y = MathUtils.random(MIN_Y, MAX_Y);
+                TimedButton toBeAdded = new TimedButton(x, y, intensity * 2f, clickEffect, false);
+                if(buttons.stream().noneMatch(tb -> toBeAdded.getHb().intersects(tb.getHb()))) {
+                    buttons.add(new TimedButton(x, y, intensity * 2f, clickEffect, false));
+                    break;
+                }
+            }
 
             timeSince = intensity/2f;
         }
