2961818422b560509cd86610854bc0147421a907
erasels <emanuelbedburdick@yahoo.de>
2019-10-06 11:39:09 +0200

    Update SpeedClickEnemyTime.java

--- theSpeedster/src/main/java/theSpeedster/mechanics/speed/SpeedClickEnemyTime.java
+++ theSpeedster/src/main/java/theSpeedster/mechanics/speed/SpeedClickEnemyTime.java
@@ -16,7 +16,7 @@ public class SpeedClickEnemyTime extends AbstractSpeedTime {
     }
 
     @Override
-    protected void effect() {
+    public void effect() {
         if(InputHelper.justClickedLeft) {
             AbstractMonster m = UC.getAliveMonsters().stream().filter(mon -> mon.hb.hovered).findAny().orElse(null);//AlchHelper.getClosestMonster(InputHelper.mX, InputHelper.mY, 300f);
             if(m != null)
