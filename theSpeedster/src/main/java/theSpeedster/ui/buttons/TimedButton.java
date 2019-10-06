084f3f72fae6fd9af629a7671d7afae88f0fd6bc
erasels <emanuelbedburdick@yahoo.de>
2019-10-06 11:38:43 +0200

    Update TimedButton.java

--- theSpeedster/src/main/java/theSpeedster/ui/buttons/TimedButton.java
+++ theSpeedster/src/main/java/theSpeedster/ui/buttons/TimedButton.java
@@ -1,33 +1,51 @@
 package theSpeedster.ui.buttons;
 
 import basemod.ClickableUIElement;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.RarePotionParticleEffect;
 import org.apache.commons.lang3.math.NumberUtils;
+import theSpeedster.TheSpeedster;
+import theSpeedster.util.TextureLoader;
 import theSpeedster.util.UC;
+import theSpeedster.vfx.general.ButtonConfirmedEffect;
 
 import java.util.function.Consumer;
 
 public class TimedButton extends ClickableUIElement {
-    public boolean isDone;
+    public boolean isDone, ordered, lockDuration;
+    public int order;
 
-    protected float startingDuration, duration;
+    protected float startingDuration, duration, sparkleTimer;
     protected Consumer<TimedButton> clickEffect;
 
     //TODO: Make TimedButtonSpeedTime and add an arraylist fo these, iterate over them in the update and check for isDone before removing
-    public TimedButton(Texture texture, float x, float y, float timer, Consumer<TimedButton> clickEffect) {
+    public TimedButton(Texture texture, float x, float y, float timer, Consumer<TimedButton> clickEffect, boolean ordered) {
         super(texture, x, y, texture.getWidth(), texture.getHeight());
         startingDuration = duration = timer;
+        this.ordered = ordered;
         this.clickEffect = clickEffect;
-        this.tint = Color.GOLDENROD.cpy();
+        if(ordered) {
+            this.tint = Color.FIREBRICK.cpy();
+        } else {
+            this.tint = Color.FOREST.cpy();
+        }
         isDone = false;
+    }
+
+    public TimedButton(float x, float y, float timer, Consumer<TimedButton> clickEffect, boolean ordered) {
+        this(TextureLoader.getTexture(TheSpeedster.makeUIPath("NormalButton.png")), x, y, timer, clickEffect, ordered);
     }
 
     @Override
     protected void onHover() {
-        //Could make button order a thing and make it tint red when this isn't the next in the order
-        tint.a = 0.75f;
+        if(!ordered || order != 0) {
+            tint.a = 0.75f;
+        }
     }
 
     @Override
@@ -37,21 +55,28 @@ public class TimedButton extends ClickableUIElement {
 
     @Override
     protected void onClick() {
+        UC.doVfx(new ButtonConfirmedEffect(x, y));
         clickEffect.accept(this);
+        isDone = true;
+        setClickable(false);
+        lockDuration = true;
     }
 
     public float getPercentageTimeLeft() {
         return NumberUtils.min(duration/startingDuration, 0f);
     }
 
-    @Override
-    public void update() {
+    public void update(int order) {
+        this.order = order;
         super.update();
 
-        duration -= UC.gt();
-        if(duration < 0) {
+        if(!lockDuration) {
+            duration -= UC.gt();
+        }
+        if(duration < 0 && !lockDuration) {
             isDone = true;
             setClickable(false);
+            lockDuration = true;
         }
     }
 
@@ -59,5 +84,29 @@ public class TimedButton extends ClickableUIElement {
     public void render(SpriteBatch sb, Color col) {
         col.a = getPercentageTimeLeft();
         super.render(sb, col);
+
+        if(ordered) {
+            //TODO: Make number render on button
+        }
+
+        if(!ordered || order != 0) {
+            renderParticles(sb);
+        }
+    }
+
+    public void renderParticles(SpriteBatch sb) {
+        this.sparkleTimer -= Gdx.graphics.getDeltaTime();
+        if (this.sparkleTimer < 0.0F) {
+            float xOffset = MathUtils.random(0, hb_w/2f);
+            float yOffset = MathUtils.random(0, hb_h/2f);
+            if(MathUtils.randomBoolean()) {
+                xOffset = -xOffset;
+            }
+            if(MathUtils.randomBoolean()) {
+                yOffset = -yOffset;
+            }
+            AbstractDungeon.topLevelEffects.add(new RarePotionParticleEffect(x+xOffset, y+yOffset));
+            this.sparkleTimer = MathUtils.random(0.2F, 0.4F);
+        }
     }
 }
